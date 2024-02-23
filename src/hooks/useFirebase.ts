31ec48d46f390ed0346ab757817116c114354c0e
withoutspaces <dms@ic.ufal.br>
2024-02-22 22:14:56 -0300

    Update useFirebase.ts to include new functions for adding and retrieving questions

--- src/hooks/useFirebase.ts
+++ src/hooks/useFirebase.ts
@@ -1,11 +1,34 @@
-import { addDoc, collection } from 'firebase/firestore';
+import { addDoc, getDocs, collection, where, query } from 'firebase/firestore';
 import { db } from "@/services/firebase"; 
 
-export function useFirebase() {
-    const questionsRef = collection(db, "questions");
+import type { Category, QuestionData } from '@/types/questions';
 
-    const addQuestion = async (data: any) => {
-        await addDoc(questionsRef, data);
+export function useFirebase() {
+    
+    const addQuestion = async (data: QuestionData) => {
+        const questionsRef = collection(db, `questions-test/${data.category}/${data.difficulty}`)
+        const querySnapshot = await getDocs(query(
+            questionsRef,
+            where("question", "==", data.question),
+            where("correctAnswer", "==", data.correctAnswer),
+        ))
+        if(!querySnapshot.empty) {
+            throw new Error("Essa questão já existe!")
+        }
+        await addDoc(questionsRef, data)
     }
 
+    const getQuestions = async ({category, difficulty}: Category): Promise<{id: string, data: QuestionData}[]> => {
+        
+        const questionsRef = collection(db, `questions/${category}/${difficulty}`)
+        const snapshot = await getDocs(questionsRef)
+        return snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data() as QuestionData
+        }))
+
+    }
+
+
+    return { addQuestion, getQuestions }
 }
