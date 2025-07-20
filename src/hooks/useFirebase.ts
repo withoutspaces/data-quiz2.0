b90d667e9f02e324333f47d63f2be828374d802e
withoutspaces <dms@ic.ufal.br>
2025-07-20 11:41:52 -0300

    remove o hook do firebase enquanto não é implementado

--- src/hooks/useFirebase.ts
+++ src/hooks/useFirebase.ts
@@ -1,34 +1,33 @@
-import { addDoc, getDocs, collection, where, query } from 'firebase/firestore';
-import { db } from "@/services/firebase"; 
+// import { addDoc, getDocs, collection, where, query } from 'firebase/firestore';
+// import { db } from "@/services/firebase";
 
-import type { Category, QuestionData } from '@/types/questions';
+// import type { Category, Question } from '@/types/questions';
 
-export function useFirebase() {
-    
-    const addQuestion = async (data: QuestionData) => {
-        const questionsRef = collection(db, `questions-test/${data.category}/${data.difficulty}`)
-        const querySnapshot = await getDocs(query(
-            questionsRef,
-            where("question", "==", data.question),
-            where("correctAnswer", "==", data.correctAnswer),
-        ))
-        if(!querySnapshot.empty) {
-            throw new Error("Essa questão já existe!")
-        }
-        await addDoc(questionsRef, data)
-    }
+// export function useFirebase() {
 
-    const getQuestions = async ({category, difficulty}: Category): Promise<{id: string, data: QuestionData}[]> => {
-        
-        const questionsRef = collection(db, `questions/${category}/${difficulty}`)
-        const snapshot = await getDocs(questionsRef)
-        return snapshot.docs.map((doc) => ({
-            id: doc.id,
-            data: doc.data() as QuestionData
-        }))
+//     const addQuestion = async (data: Question) => {
+//         const questionsRef = collection(db, `questions-test/${data.category}/${data.difficulty}`)
+//         const querySnapshot = await getDocs(query(
+//             questionsRef,
+//             where("question", "==", data.question),
+//             where("correctAnswer", "==", data.correctAnswer),
+//         ))
+//         if(!querySnapshot.empty) {
+//             throw new Error("Essa questão já existe!")
+//         }
+//         await addDoc(questionsRef, data)
+//     }
 
-    }
+//     const getQuestions = async ({category, difficulty}): Promise<{id: string, data: Question}[]> => {
 
+//         const questionsRef = collection(db, `questions/${category}/${difficulty}`)
+//         const snapshot = await getDocs(questionsRef)
+//         return snapshot.docs.map((doc) => ({
+//             id: doc.id,
+//             data: doc.data() as Question
+//         }))
 
-    return { addQuestion, getQuestions }
-}
+//     }
+
+//     return { addQuestion, getQuestions }
+// }
