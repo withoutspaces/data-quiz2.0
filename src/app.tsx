1595e525ad0a50a99f0e9c2356abfd435b675f94
withoutspaces <dms@ic.ufal.br>
2025-07-21 00:00:05 -0300

    fix: altera a importação do pacote da Vercel de next para react

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
 import { Router } from "./routes/router";
-import { Analytics } from "@vercel/analytics/next";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   return (
