4068935096513bf5eca9bb3d43a7fc2028aa2c1c
withoutspaces <dms@ic.ufal.br>
2025-07-20 23:45:15 -0300

    feat: adiciona pacotes da Vercel para controle e analises

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './app.tsx'
-import './index.css'
-import { Toaster } from 'sonner'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app.tsx";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import "./index.css";
+import { Toaster } from "sonner";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-    <Toaster richColors position='top-right' />
-  </React.StrictMode>,
-)
+    <Analytics />
+    <SpeedInsights />
+    <Toaster richColors position="top-right" />
+  </React.StrictMode>
+);
