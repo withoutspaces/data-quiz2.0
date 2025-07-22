2e6f4664d562330958fd37dea83ec8b8f74f3512
withoutspaces <dms@ic.ufal.br>
2025-07-21 21:52:17 -0300

    chore: componetiza os contribuntes, mas não utiliza por enquanto

---
+++ src/components/quiz/contributors.tsx
@@ -0,0 +1,26 @@
+// interface ContributorsProps {}
+
+// export function Contributors({}: ContributorsProps) {
+//   const contributors: string[] = [];
+
+//   return (
+//     <div>
+//       {contributors.length > 0 && (
+//         <div className="absolute -bottom-64 sm:-bottom-44 lg:-bottom-32 md:-bottom-40">
+//           <div className="flex flex-col items-center gap-2 px-6 mb-8">
+//             <p className="text-indigo-400 text-sm">
+//               💓 Agradecimento especial a:
+//             </p>
+//             <ul className="flex flex-wrap justify-center gap-x-6 gap-y-3 my-2">
+//               {contributors.map((contributor, index) => (
+//                 <li key={index} className="text-indigo-400 text-sm list-disc">
+//                   {contributor}
+//                 </li>
+//               ))}
+//             </ul>
+//           </div>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
