37f315d003eb7008d3f945a77e7f4f3afb871efb
withoutspaces <dms@ic.ufal.br>
2025-07-29 18:37:07 -0300

    feat: adiciona lógica do botão no componente

--- src/components/quiz/backButton.tsx
+++ src/components/quiz/backButton.tsx
@@ -1,12 +1,15 @@
-interface BackButtonProps {
-  onClick: () => void;
-}
+import { useNavigate } from "react-router-dom";
 
-export function BackButton({ onClick }: BackButtonProps) {
+export function BackButton() {
+  const navigate = useNavigate();
+  const handlePageBack = () => {
+    navigate("/");
+  };
+
   return (
     <button
       className="text-indigo-200 absolute top-4 left-4 font-semibold hover:text-indigo-400 hover:line"
-      onClick={onClick}
+      onClick={handlePageBack}
     >
       ◀️ Voltar
     </button>
