c19977910b91bd4417bfd3bd115db14906f1439d
withoutspaces <dms@ic.ufal.br>
2025-07-29 18:37:35 -0300

    chore: remove a tag que impedia de usar o componente em outra tela

--- src/components/quiz/scoreDisplay.tsx
+++ src/components/quiz/scoreDisplay.tsx
@@ -3,27 +3,19 @@ interface ScoreDisplayProps {
   currentQuestion: number;
 }
 
-export function ScoreDisplay({
-  currentQuestion,
-  currentScore,
-}: ScoreDisplayProps) {
+export function ScoreDisplay({ currentQuestion, currentScore }: ScoreDisplayProps) {
   const incorrectAnswers = currentQuestion - currentScore;
 
   return (
     <div className="flex gap-4 items-center">
-      <span className="font-semibold">Questão: {currentQuestion + 1}/10</span>
-      {currentQuestion >= 0 && (
-        <>
-          <div className="ring-1 ring-indigo-400 py-1 pl-1 pr-2 flex gap-1 rounded-sm">
-            <span>✅</span>
-            <p>{currentScore}</p>
-          </div>
-          <div className="ring-1 ring-indigo-400 py-1 pl-1 pr-2 flex gap-1 rounded-sm">
-            <span>❌</span>
-            <p>{incorrectAnswers < 0 ? 0 : incorrectAnswers}</p>
-          </div>
-        </>
-      )}
+      <div className="ring-1 ring-indigo-400 py-1 pl-1 pr-2 flex gap-1 rounded-sm">
+        <span>✅</span>
+        <p>{currentScore}</p>
+      </div>
+      <div className="ring-1 ring-indigo-400 py-1 pl-1 pr-2 flex gap-1 rounded-sm">
+        <span>❌</span>
+        <p>{incorrectAnswers < 0 ? 0 : incorrectAnswers}</p>
+      </div>
     </div>
   );
 }
